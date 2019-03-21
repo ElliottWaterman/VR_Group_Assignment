09af99bb1edd0254a7f4e455b473d4273bd290ca
GeorgieChallis <g.l.challis@student.reading.ac.uk>
2019-03-21 04:15:08 +0000

    Test: NOVR grabber

---
+++ Airplane_Simulation/Assets/Scripts/NOVR_Grabbable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NOVR_Grabbable : MonoBehaviour
+{
+   // public Transform holdLocation;
+    private static bool grabbed;
+
+    private void OnMouseDown()
+    {
+        if (!grabbed)
+        {
+            Debug.Log("Click On");
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Collider>().enabled = false;
+            transform.parent = GameObject.Find("FPSController(Clone)").transform;
+            grabbed = true;
+        }
+        else {
+            Debug.Log("Click Off");
+            transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Collider>().enabled = true;
+            grabbed = false;
+        }
+    }
+}
